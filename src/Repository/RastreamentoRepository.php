7b389533eec00f3e05a611b4157024d7fcaf120f
br3cht <ant_175@hotmail.com>
2025-01-25 20:25:13 -0300

    refactor: added methods for filter query

--- src/Repository/RastreamentoRepository.php
+++ src/Repository/RastreamentoRepository.php
@@ -3,6 +3,7 @@
 namespace Src\Repository;
 
 use Database;
+use Exception;
 
 class RastreamentoRepository
 {
@@ -10,23 +11,57 @@ class RastreamentoRepository
 
     public function __construct()
     {
-        $this->database = New Database();
+        $this->database = new Database();
     }
 
-    public function getTracking()
+    public function getTracking(array $filters): array
     {
 
-        if(!$this->database->connect()){
-            return null;
+        if (!$this->database->connect()) {
+            throw new Exception('');
         }
 
         $data = $this->database->sqlQueryArray(
             "SELECT
              *
             FROM rastreamentos
+            {$this->filterByPlaca($filters['placa'])}
+            {$this->filterByFuncionario($filters['funcionario'])}
+            {$this->filterByDataOcorrencia($filters['data_inicial'], $filters['data_final'])}
             "
         );
 
+        if(!is_array($data)){
+            return [];
+        }
+
         return $data;
+    }
+
+    private function filterByPlaca(string|null $placa): string
+    {
+        if($placa){
+            return "WHERE placa_veiculo LIKE '%{$placa}%'";
+        }
+
+        return '';
+    }
+
+    private function filterByFuncionario(string|null $funcionario): string
+    {
+        if($funcionario){
+            return "WHERE nome_funcionario LIKE '%{$funcionario}%'";
+        }
+
+        return '';
+    }
+
+    private function filterByDataOcorrencia(string|null $dateStart, string|null $dateEnd): string
+    {
+        if(empty($dateStart) || empty($dateEnd)) {
+            return '';
+        }
+
+        return "WHERE DATE data_ocorrencia BETWEEN' %{$dateStart}%' and '%{$dateEnd}%'";
     }
 }
