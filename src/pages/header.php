81cde7293913fa2160eec4f9f39d4c022e3ee43c
br3cht <ant_175@hotmail.com>
2025-01-25 13:15:33 -0300

    refactor: change permission folder

--- src/pages/header.php
+++ src/pages/header.php
@@ -14,7 +14,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="assets/js/component.js"></script>
+    <script src="/assets/js/component.js"></script>
 </head>
 
 <body>
