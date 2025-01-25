a3c6fda8fff399eb0f5b1cd812e68832d36c466d
br3cht <ant_175@hotmail.com>
2025-01-25 16:39:40 -0300

    feat: added new method for calculate diff vel

--- src/Controller/RastreamentoController.php
+++ src/Controller/RastreamentoController.php
@@ -17,15 +17,28 @@ class RastreamentoController
     public function getAllTRackings(): string
     {
         $status = 'success';
-
         $data = $this->rastreamento->getTracking();
+
         if (empty($data)) {
             $status = 'failure';
         }
+
+        $data = $this->calculateDiff($data);
 
         return json_encode([
             'status' => $status,
             'data' => $data
         ]);
+    }
+
+    private function calculateDiff(array $data)
+    {
+        foreach($data as &$item)
+        {
+            $item['vel_diff'] = (($item['velocidade_registrada'] - $item['velocidade_maxima']) / $item['velocidade_maxima'])  * 100;
+            $item['vel_diff'] = number_format($item['vel_diff'], 2) . '%';
+        }
+
+        return $data;
     }
 }
