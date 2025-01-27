e13ee5b9ef6d1ae3435b48804375b076f614b27c
br3cht <ant_175@hotmail.com>
2025-01-26 23:56:47 -0300

    feat: added vendor for use only php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/www',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e5c98dbf73d3a9605cc77f002394d994bad7fd63',
+        'type' => 'yes',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/www' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e5c98dbf73d3a9605cc77f002394d994bad7fd63',
+            'type' => 'yes',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
